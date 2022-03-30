94ec83a320825264b0236e03fe6763585399a801
Chibueze Kalu <kalu.chibueze@hotmail.com>
2022-03-30 16:50:39 +0200

    completed tutorial

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBV5RJwlxpGoe8en_Vz7uV9UeEu2RG1Ga8",
-  authDomain: "fir-app-a710e.firebaseapp.com",
-  projectId: "fir-app-a710e",
-  storageBucket: "fir-app-a710e.appspot.com",
-  messagingSenderId: "277573945401",
-  appId: "1:277573945401:web:bc834c372e5cd90a8a25e8",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 // initialize firebase app
 const app = initializeApp(firebaseConfig);
